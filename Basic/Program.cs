14d7771f649c230e2f19438bf3b4e09ac7961ca2
korbat4625 <korbat4625@gmail.com>
2022-02-23 22:40:49 +0800

    first save

---
+++ Basic/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Basic {
+    internal class Program {
+        static void Main(string[] args) {
+            Vehicle v1 = new(
+                new BasicEngine()
+            );
+            v1._engine.SpeedUp(500);
+            v1._engine.ShowSpeed();
+
+            Console.WriteLine("=================");
+
+            Animal animal = new Pet("Black");
+            animal.Shout();
+            Animal dog = new Dog("Doggy");
+            dog.Shout();
+        }
+        abstract class Animal {
+            abstract protected string Name { get; set; }
+            abstract public void Scared();
+            abstract public void Shout();
+        }
+        class Pet : Animal {
+            protected override string Name { get; set; }
+            public Pet(string name) {
+                Name = name;
+            }
+            public override void Scared() {
+                Console.WriteLine("Pet scared!");
+            }
+
+            public override void Shout() {
+                Console.WriteLine("My name is:" + Name);
+            }
+        }
+
+        class Dog : Pet, Animal {
+            public Dog(string name):base(name) {
+                Name = name;
+            }
+            public override void Shout() {
+                Console.WriteLine("My name is:" + Name);
+            }
+        }
+
+
+        class BasicEngine : IEngine {
+            public int power = 0;
+
+            public void SpeedDown(int num) {
+                power -= num;
+            }
+
+            public virtual void SpeedUp(int num) {
+                power += num;
+            }
+            public void ShowSpeed() {
+                Console.WriteLine(power);
+            }
+        }
+
+        class SuperEngine : BasicEngine {
+            public override void SpeedUp(int num) {
+                this.power = (num + 200);
+            }
+        }
+    }
+}
---
+++ Basic/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic {
+    interface IEngine {
+        public void SpeedUp(int num);
+        public void SpeedDown(int num);
+        public void ShowSpeed();
+    }
+    internal class Vehicle {
+        public IEngine _engine;
+        public Vehicle(IEngine engine) {
+            _engine = engine;
+        }
+    }
+}
